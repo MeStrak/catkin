c6fcf09f7f7ad2460a8d5eacb22dad6837444f0c
Strak <j.strachan@yahoo.co.uk>
2020-02-01 18:06:09 +0100

    fix: Fixed personas.resolver.spec.ts dependencies

--- backend/src/personas/personas.resolver.spec.ts
+++ backend/src/personas/personas.resolver.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonasResolver } from './personas.resolver';
+import { PersonasService } from './personas.service';
+import { getModelToken } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { PersonaSchema } from './persona.schema';
 
 describe('PersonasResolver', () => {
   let resolver: PersonasResolver;
+  const personaModel = mongoose.model('PersonaSchema', PersonaSchema);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PersonasResolver],
+      providers: [
+        PersonasResolver,
+        PersonasService,
+        {
+          provide: getModelToken('Persona'),
+          useValue: personaModel,
+        },
+      ],
     }).compile();
 
     resolver = module.get<PersonasResolver>(PersonasResolver);
