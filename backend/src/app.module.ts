660010b2e103dee66474a87c729fabd5309ddd93
mestrak <mestrak@github.com>
2020-11-11 17:39:28 +0100

    feat: add new relic for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { BoardsModule } from './board/boards.module';
 import { GroupsModule } from './group/groups.module';
 import { ItemsModule } from './items/items.module';
 import { PersonasModule } from './personas/personas.module';
+const newRelicPlugin = require('@newrelic/apollo-server-plugin')
+
 
 @Module({
   imports: [
@@ -17,6 +19,7 @@ import { PersonasModule } from './personas/personas.module';
       playground: true,
       introspection: true,
       context: ({ req }) => ({ req }),
+      plugins: [newRelicPlugin],
     }),
     ItemsModule,
     PersonasModule,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,9 +11,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors();
+  require('newrelic');
   const PORT = process.env.PORT || 3000;
   Logger.log('Setting port: ' + PORT, 'info');
   Logger.log('Auth0 domain: ' + process.env.AUTH0_DOMAIN, 'Auth0');
+  Logger.log('New Relic app name: ' + process.env.NEW_RELIC_APP_NAME, 'newrelic');
 
   await app.listen(PORT);
 }
