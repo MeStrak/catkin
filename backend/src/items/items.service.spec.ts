a6a5461022d20b93640620c4b675346d8f20295f
MeStrak <mestrak@github.com>
2020-12-20 22:27:03 +0000

    fix: item tests

--- backend/src/items/items.service.spec.ts
+++ backend/src/items/items.service.spec.ts
@@ -3,15 +3,26 @@ import { ItemsService } from './items.service';
 import { getModelToken } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { ItemSchema } from './item.schema';
+import { GroupsService } from '../group/groups.service';
+import { GroupSchema } from '../group/groups.schema';
 
 describe('ItemsService', () => {
   let service: ItemsService;
 
   const itemModel = mongoose.model('ItemSchema', ItemSchema);
+  const groupModel = mongoose.model('GroupSchema', GroupSchema);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        
+      ],
       providers: [
+        GroupsService,
+        {
+          provide: getModelToken('Group'),
+          useValue: groupModel,
+        },
         ItemsService,
         {
           provide: getModelToken('Item'),
