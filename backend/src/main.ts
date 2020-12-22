6884e667d0724793bb73b2892908740e90e5d6bc
MeStrak <mestrak@github.com>
2020-12-22 14:54:19 +0000

    fix: graphql playground now loads in dev

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,12 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet());
+  app.use(helmet({ contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false }));
   app.enableCors();
+  
   require('newrelic');
   const PORT = process.env.PORT || 3000;
   Logger.log('Setting port: ' + PORT, 'info');
   Logger.log('Auth0 domain: ' + process.env.AUTH0_DOMAIN, 'Auth0');
+  Logger.log('New Relic enabled: ' + process.env.NEW_RELIC_ENABLED, 'newrelic');
   Logger.log('New Relic app name: ' + process.env.NEW_RELIC_APP_NAME, 'newrelic');
 
   await app.listen(PORT);
