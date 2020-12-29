bb872ca9238defb15264fad9e2a9d4cf0df9b117
ayankovsky <p44gr10@gmail.com>
2020-12-29 22:58:22 +0100

    chore: added basic recap.dev support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
+import { trace } from './recap.dev';
+
+trace();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,13 +14,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet({ contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false }));
   app.enableCors();
-  
+
   require('newrelic');
   const PORT = process.env.PORT || 3000;
   Logger.log('Setting port: ' + PORT, 'info');
   Logger.log('Auth0 domain: ' + process.env.AUTH0_DOMAIN, 'Auth0');
   Logger.log('New Relic enabled: ' + process.env.NEW_RELIC_ENABLED, 'newrelic');
   Logger.log('New Relic app name: ' + process.env.NEW_RELIC_APP_NAME, 'newrelic');
+  Logger.log('recap.dev enabled: ' + process.env.RECAP_DEV_ENABLED, 'recap.dev');
 
   await app.listen(PORT);
 }
---
+++ backend/src/recap.dev.ts
@@ -0,0 +1,9 @@
+import { traceExpress, captureConsoleLogs } from '@recap.dev/client'
+import express from 'express'
+
+export const trace = () => {
+  if (process.env.RECAP_DEV_ENABLED && process.env.RECAP_DEV_ENABLED === 'true') {
+    traceExpress(express)
+    captureConsoleLogs()
+  }
+}
