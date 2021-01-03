7df36f169ee17af4797c615c48902b7e01ebadcd
MeStrak <call_me_nigel@yahoo.co.uk>
2021-01-03 15:37:38 +0000

    fix: return empty group on no permissions

--- backend/src/users/user.helper.ts
+++ backend/src/users/user.helper.ts
@@ -2,6 +2,8 @@ import { GroupsService } from '../group/groups.service';
 
 export function GetUserGroups(user: any): string[] {
   let groups: string[] = []; // eslint-disable-line prefer-const
+  if(!user['https://catkin.dev/permissions']) return groups;
+
   user['https://catkin.dev/permissions'].forEach((element) => {
     groups.push(element.group);
   });
@@ -20,6 +22,8 @@ export function IsInGroup(user: any, group: string): boolean {
 
 export function GetWritableUserGroups(user: any): string[] {
   let groups: string[] = []; // eslint-disable-line prefer-const
+  if(!user['https://catkin.dev/permissions']) return groups;
+
   user['https://catkin.dev/permissions'].forEach((element) => {
     if (element.role === 'admin' || element.role === 'member') {
       groups.push(element.group);
@@ -30,6 +34,8 @@ export function GetWritableUserGroups(user: any): string[] {
 
 export function GetAdminUserGroups(user: any): string[] {
   let groups: string[] = [];  // eslint-disable-line prefer-const
+  if(!user['https://catkin.dev/permissions']) return groups;
+
   user['https://catkin.dev/permissions'].forEach((element) => {
     if (element.role === 'admin') {
       groups.push(element.group);
